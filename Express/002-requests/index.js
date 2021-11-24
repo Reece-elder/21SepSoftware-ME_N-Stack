e808c4d8f6897c58fd3db290e26a465f123a4bac
reece-elder <reece.elder@qa.com>
2021-11-24 14:30:22 +0000

    request and response

--- Express/002-requests/index.js
+++ Express/002-requests/index.js
@@ -2,18 +2,47 @@ const express = require('express');
 
 const app = express();
 
+// Add Custom Middleware to convert data
+app.use(express.json());
+
 // adding custom requests
 
 // When a user goes to '/' 
 // it will print to the console "site has been accessed"
-app.get('/', () => {
-    console.log("Site has been accessed!");
-});
+// app.get('/', () => {
+//     console.log("Site has been accessed!");
+// });
 
 app.get('/hello', () => {
     console.log("hello, you've accessed the site!");
 })
 
+app.get('/getAll', () => {
+    console.log("Get all request");
+})
+
+// HTTP Requests take in a REQ (Request) and a RES (Response)
+// Request - ID Params, body, search terms 
+// Response - What is returned from the database, backend to the front end
+
+// To add a query param (such as id) use :variable name
+app.get('/get/:id', (req, res) => {
+    console.log(req);
+    console.log(req.params.id);
+
+    // This will post some data to the front end
+    res.send(`Id ${req.params.id} has been passed :o`);
+});
+
+// Pass in a body as a request 
+// To pass in a body you must use postman and we need some middleware
+app.post('/newSnake', (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    res.status(202).send(body);
+    // You can specify the status code with res.status
+});
 
 
 // Telling our app to run on a port 
---
+++ Express/002-requests/index.md
@@ -0,0 +1,11 @@
+Exercise
+
+Using your basic Requests (getAll, create, update etc)
+Add req,res to each of these with the following syntax
+
+Delete - Takes in ID, prints out "object of id deleted"
+Get/byid - Takes in the id and prints out an object already
+Create - Takes in a body and prints it out
+Update - takes in id and a body, prints them both out
+
+Give each request an appropriate res.status
