3055dcb2d0b5f9e71e83124b233f2b6e2309cb56
reece-elder <reece.elder@qa.com>
2021-11-25 12:47:01 +0000

    basic CRUD

--- Express/005-MongooseDemo/routes/zoo.js
+++ Express/005-MongooseDemo/routes/zoo.js
@@ -17,11 +17,66 @@ router.post('/create', (req, res) => {
 
     // Save the lizard into our database
     // .save() is an inbuilt function to add the lizard to the mongodb
-    lizard.save().then((result) => {
-        res.status(201).send("Lizard saved to database!");
+    lizard.save()
+    .then((result) => {
+        res.status(201).send(`${result} saved to database!`);
+    })
+    .catch((error) => {
+        console.log(`error :( : ${error}`);
+        res.status(500).send(error);
     });
 
 });
+
+// Get All Route
+router.get('/getAll', (req, res) => {
+
+    // Use the Lizard in built functions to get all the data
+    Lizard.find((error, lizardList) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        }
+        res.status(200).send(lizardList);
+    })
+})
+
+// Get One (by id)
+router.get('/getId/:id', (req, res) => {
+    console.log(req.params.id);
+    Lizard.findById(req.params.id, (error, result) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        } 
+        res.status(200).send(result);
+    })
+})
+
+// Get by Name
+router.get('/getName/:name', (req, res) => {
+    const name = req.params.name;
+    console.log(name);
+    Lizard.find({"name" : name}, (error, result) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        } 
+        res.status(200).send(result);
+    });
+});
+
+// Delete By ID
+router.delete('/deleteId/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    Lizard.findByIdAndDelete(id, (error) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        } 
+        res.status(202).send("Deleted!");
+    });
+});
+
+// Exercise - Add all of the basic CRUD (Create, ReadAll, ReadOne, Update, Delete)
+// using appropriate responses and status codes
 
 
 
