78fd43164e392bb0d6221c81797a6aadd50f8990
reece-elder <reece.elder@qa.com>
2021-11-26 09:35:51 +0000

    basic chai testing

--- Express/006-express-testing/tests/intro.test.js
+++ Express/006-express-testing/tests/intro.test.js
@@ -3,29 +3,37 @@
 // it() - used for an actual test
 
 // This test suite takes in a 'String' to describe the suite AND an arrow function for the tests
-describe('Testing if basic math works', function() {
+// describe('Testing if basic math works', function() {
 
-    // To make the tests more semantic, 
-    // Trying to mimic user stories
-    it('Should print the word hello', function() {
-        console.log("Hello");
-    });
+//     // To make the tests more semantic, 
+//     // Trying to mimic user stories
+//     it('Should print the word hello', function() {
+//         console.log("Hello");
+//     });
 
-    it('Should print the number 5', function() {
-        console.log(5);
-    });
+//     it('Should print the number 5', function() {
+//         console.log(5);
+//     });
 
-    // Assertion libraries let you do things like 
-    // expect(outcome).to.be(5);
+//     // Assertion libraries let you do things like 
+//     // expect(outcome).to.be(5);
 
-    before(function() {
-        console.log("This will run first!");
-    });
+//     before(function() {
+//         console.log("To add some dummy data to our repo in the first place");
+//     });
 
-    after(function() {
-        console.log("This will run last!");
-    });
+//     after(function() {
+//         console.log("Clear data from the database");
+//     });
 
-});
+//     beforeEach(function() {
+//         console.log("This will run before each test");
+//     })
+
+//     afterEach(function() {
+//         console.log("This will run after each test");
+//     })
+
+// });
 
 // Exercise - Create a test suite with 2 'tests' as well as using a before and after function 
